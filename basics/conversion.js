a294e20d03a86827b93755b240de686f8eb1e74f
Ojasvi Bakshi <86290251+Ojasvix20@users.noreply.github.com>
2025-02-27 18:49:13 +0000

    update

---
+++ basics/conversion.js
@@ -0,0 +1,31 @@
+/* let score = "33";
+
+console.log(typeof(score))
+
+let valueInNumber= Number(score)
+console.log(typeof(valueInNumber)) */
+
+// conversion of x to number:
+// String ("33") to no. => 33
+// String("abc" or "123abc") to no. => NaN
+// null to no. => 0
+// undefined to no. => NaN
+// boolean to no. => 1 or 0
+
+
+/* let isloggedIn=0
+let booleanIsLoggedIn= Boolean(isloggedIn)
+
+console.log(typeof(booleanIsLoggedIn))
+console.log(booleanIsLoggedIn) */
+
+// from x to boolean:
+// 1=> true, 0=> false
+// ""=> false
+// "xyz"=> true
+
+
+let someNumber= 69
+let StringSomeNumber= String(someNumber)
+console.log(typeof(StringSomeNumber))
+console.log(StringSomeNumber)
