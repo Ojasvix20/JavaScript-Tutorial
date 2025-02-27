d32a6a67843cb62f83f176e06720c3a36fef359f
Ojasvi Bakshi <86290251+Ojasvix20@users.noreply.github.com>
2025-02-27 20:01:13 +0000

    6416

---
+++ basics/comparison.js
@@ -0,0 +1,33 @@
+// console.log(2>1);
+// console.log("2">1);
+// console.log("02">1);
+
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log(null==0);
+// console.log(null>0);
+// console.log(null>=0);
+// console.log(null<=0);
+
+// comparison operator >, <, >=, <= convert null to 0; but == doesnt do that
+
+// console.log(undefined==0);
+// console.log(undefined>0);
+// console.log(undefined>=0);
+// console.log(undefined!=0);
+
+// undefined always gives false when comparing
+
+
+// *** in JavaScipt >= (comparision) & == work differently when comparing for equality
+
+// we have strict check (===) also checks data type as well as value
+
+console.log("2"==2);
+console.log("2"===2);
+
+
