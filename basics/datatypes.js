cbdee94a585750c492f7cfcfe4a2a0dae433d4b1
Ojasvi Bakshi <86290251+Ojasvix20@users.noreply.github.com>
2025-02-27 18:20:42 +0000

    53yyeh

---
+++ basics/datatypes.js
@@ -0,0 +1,23 @@
+// Tutorial - 02
+
+"use strict";  //use all JS code as newer version (not mandatory)
+
+//alert(23+46) //we are using node js thus error, it will work in webpage
+
+console.log(3+3)
+
+// DATATYPES:
+// number (2 to power 53)
+// bigInt
+// string ("")
+// boolean (true or false)
+// null => standalone value
+// undefined 
+// symbol =>unique
+
+// object
+
+console.log(typeof "ramesh")
+console.log(typeof null)   //object
+console.log(typeof undefined)   //undefined
+console.log(typeof 123)
