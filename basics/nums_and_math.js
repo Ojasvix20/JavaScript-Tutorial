5a4aa5d939ecfbd6fbbe8f89a4ff4d0442f6716a
Ojasvi Bakshi <86290251+Ojasvix20@users.noreply.github.com>
2025-03-01 10:06:18 +0000

    up

--- basics/nums_and_math.js
+++ basics/nums_and_math.js
@@ -17,3 +17,21 @@ const anotherNumber = 100000;
 
 // *******************Maths******************************
 
+// console.log(Math);
+// console.log(Math.abs(-40));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.6));    //higher side round off
+// console.log(Math.floor(4.9));   //lower side round off
+
+// console.log(Math.sqrt(49));
+// console.log(Math.min(4,5,7,3,2,1));
+// console.log(Math.max(4,5,7,3,2,1));
+
+console.log(Math.random()); //returns random no. between 0 & 1
+
+console.log(Math.random() * 10 + 1); //minimum 1, max 9
+console.log(Math.floor(Math.random() * 10 + 1)); //minimum 1, max 9
+
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random()*(max-min+1))+min);
--- basics/tempCodeRunnerFile.js
+++ basics/tempCodeRunnerFile.js
@@ -1,3 +1,9 @@
+;
+// console.log(Math.abs(-40));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.6));    //higher side round off
+// console.log(Math.floor(4.9));   //lower side round off
 
-// console.log("2">1);
-// console.log("02">
+// console.log(Math.sqrt(49));
+// console.log(Math.min(4,5,7,3,2,1));
+// console.log(Math.max(4,5,7,3,2,1));
