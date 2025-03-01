3b40efc4175b5ff7999901f01e69b42b750d24e1
Ojasvi Bakshi <86290251+Ojasvix20@users.noreply.github.com>
2025-03-01 09:56:09 +0000

    update

---
+++ basics/nums_and_math.js
@@ -0,0 +1,19 @@
+const score = 400;
+// console.log(score);
+
+const balance = new Number(10000); //explicitly telling the datatype
+
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 1123.8966;
+// console.log(otherNumber.toPrecision(3));
+
+const anotherNumber = 100000;
+// console.log(anotherNumber.toLocaleString('en-IN'));
+
+// *******************Maths******************************
+
