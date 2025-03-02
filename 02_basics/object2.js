a9bcffe0e053e6e967b853aa916da13e3c5e430c
Ojasvi Bakshi <86290251+Ojasvix20@users.noreply.github.com>
2025-03-02 19:22:43 +0000

    update

---
+++ 02_basics/object2.js
@@ -0,0 +1,40 @@
+const tinderUser = new Object();
+
+tinderUser.id = "123abc";
+tinderUser.name = "Ramesh";
+tinderUser.isLoggedIn = false;
+
+// console.log(tinderUser);
+
+const regularUser = {
+  email: "User@gmail.com",
+  fullName: {
+    userFullName: {
+      firstName: "Tunnu",
+      lastName: "Lal",
+    },
+  },
+};
+
+// console.log(regularUser.fullName.userFullName);
+// console.log(regularUser.fullName.userFullName.firstName);
+
+const obj1 = { 1: "a", 2: "b", 3: "c" };
+const obj2 = { 4: "d", 5: "e", 6: "f" };
+
+// const obj3= {obj1,obj2}
+// console.log(obj3);
+
+//combine 2 or more objects correctly:
+
+// const obj3 = Object.assign({},obj1,obj2);
+// console.log(obj3);
+
+// using spread operator:
+const obj3 = { ...obj1, ...obj2 };
+// console.log(obj3);
+
+// to return th keys of an object:
+
+console.log(Object.keys(tinderUser));
+
