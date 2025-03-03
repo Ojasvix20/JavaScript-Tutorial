a47a751c5053a57d468aa6e07433f0de56f9a517
Ojasvi Bakshi <86290251+Ojasvix20@users.noreply.github.com>
2025-03-03 10:56:52 +0000

    update

--- 03_basics/functions.js
+++ 03_basics/functions.js
@@ -31,7 +31,44 @@ function greeting(name = "Tunnu") {
   return `${name}, Hello Good Morning`;
 }
 
-console.log(greeting());        //will use the default value
-console.log(greeting("Ramesh"));    //will use the argument value
+console.log(greeting()); //will use the default value
+console.log(greeting("Ramesh")); //will use the argument value
 
+//using rest operator
+//rest operator looks same as spread operator.
+function calculateCartPrice(...num1) {
+  return num1;
+}
+console.log(calculateCartPrice(200, 300, 400, 300));
+
+// function w objects
+
+const user = {
+  username: "Hitesh",
+  price: 99,
+};
+function handleObjects(anyObject) {
+  console.log(
+    `Username is ${anyObject.username} and price is ${anyObject.price}`
+  );
+}
+
+handleObjects(user);
+// we can also pass argument object directly
+handleObjects({
+  username: "Ray",
+  price: 699,
+});
+
+// passing array as argument
+
+const myArr = [200, 300, 400, 800];
+
+function secondArrValue(getArray) {
+  return getArray[1];
+}
+
+console.log(secondArrValue(myArr));
+// we can also pass array directly
+console.log(secondArrValue([100,90,80,40]));
 
