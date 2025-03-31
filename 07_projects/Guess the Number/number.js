2b6c2fe9ee0d87a1dfa9f0824043898117d57f91
Ojasvi Bakshi <86290251+Ojasvix20@users.noreply.github.com>
2025-04-01 00:11:18 +0530

    Add files via upload

---
+++ 07_projects/Guess the Number/number.js
@@ -0,0 +1,67 @@
+const randomNo = (parseInt((Math.random()) * 100 + 1))
+
+const submit = document.querySelector('#subt');
+const userInput = document.querySelector('#guessField');
+const guessSlot = document.querySelector('.guesses')
+const remaining = document.querySelector('.lastResult');
+
+const lowOrHi = document.querySelector('.lowOrHi');
+const startOver = document.querySelector('.resultParas');
+
+const p = document.createElement('p')
+
+let prevGuess = []
+let numGuess = 1;
+
+let playGame = true;
+
+if (playGame) {
+    submit.addEventListener('click', function (e) {
+        e.preventDefault()
+        const guess = parseInt(userInput.value);
+        console.log(guess);
+        validateGuess(guess);
+    })
+}
+
+function validateGuess(guess) {
+    if (isNaN(guess)) {
+        alert("Please enter a valid number!");
+    } else if (guess < 1) {
+        alert("Please enter a number greater than 1!");
+    } else if (guess < 100) {
+        alert("Please enter a number less than 100!");
+    } else {
+        prevGuess.push(guess);
+        if (numGuess === 11) {
+            displayGuess(guess);
+            displayMessage(`Game Over, Answe was ${randomNo}`);
+            endGame();
+        } else {
+            displayGuess(guess);
+            checkGuess(guess);
+        }
+    }
+}
+
+function checkGuess(guess) {
+    
+}
+
+function displayGuess(guess) {
+    // 
+}
+
+function displayMessage(message) {
+    // 
+}
+
+function endGame() {
+    // 
+}
+
+function newGame() {
+    // 
+}
+
+// continue from 1:13:25
