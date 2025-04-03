d46985d0851a3e55d9598c5f402ae06243aedb8a
Ojasvi Bakshi <86290251+Ojasvix20@users.noreply.github.com>
2025-04-03 22:28:59 +0530

    Add files via upload

---
+++ 08_events/events_basics.js
@@ -0,0 +1,17 @@
+// Javascript is a sequentially run language but there are exceptions
+
+// document.getElementById('owl').onclick=function () {
+//     alert("owl clicked")
+// }
+//we use event listeneres instead of functions like onclick because thats more modern and includes more features.
+
+// AN EVENT LISTENER:
+
+// document.getElementById("owl").addEventListener("click", function (params) {
+//   alert("Owl clicked");
+// });
+
+//event listener is the best approach amongst all others, however we have few other approaches as well like:
+// attachEvent -  old way during the time of internet eplorer
+// JQuery - on
+
