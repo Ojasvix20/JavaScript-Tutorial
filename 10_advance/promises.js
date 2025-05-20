6e11dd4416fda43fbbd787693343a9dc9c6a0c86
Ojasvi Bakshi <86290251+Ojasvix20@users.noreply.github.com>
2025-05-21 01:01:21 +0530

    Update promises.js

--- 10_advance/promises.js
+++ 10_advance/promises.js
@@ -1,25 +1,47 @@
-const promise1 = new Promise(function(resolve, reject){
-    //things that can be done using promises:
-    //Do an async task
-    // DB calls, cryptography, network
-    
-    setTimeout(function() {
-        console.log('Async task is complete');
-        resolve()
-    },1000)
+const promise1 = new Promise(function (resolve, reject) {
+  //things that can be done using promises:
+  //Do an async task
+  // DB calls, cryptography, network
+
+  setTimeout(function () {
+    console.log("Async task is complete");
+    resolve();
+  }, 1000);
 });
 
 promise1.then(function () {
-    console.log("promise consumed.");
-    
-})
+  console.log("promise consumed.");
+});
 
 new Promise(function (resolve, reject) {
-    setTimeout(function(){
-        console.log("Async task 2");
-        resolve();        
-    },1000)
-}).then(function(){
-    console.log("Async 2 resolved");
-    
+  setTimeout(function () {
+    console.log("Async task 2");
+    resolve();
+  }, 1000);
+}).then(function () {
+  console.log("Async 2 resolved");
+});
+
+const promise3 = new Promise(function (resolve, reject) {
+  setTimeout(() => {
+    resolve({ username: "Ramesh", email: "ramesh07@gmail.com" });
+  }, 1000);
+});
+promise3.then(function (user) {
+  console.log(user);
+});
+
+const promise4 = new Promise(function (resolve, reject) {
+  setTimeout(() => {
+    let error = true;
+    if (!error) {
+      resolve({ username: "Hello", password: "12345" });
+    } else {
+      reject("ERROR: Something went wrong");
+    }
+  }, 1000);
+});
+
+promise4.then((user)=>{
+
 })
